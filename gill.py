338edfd8d126569e61fa79b7afaebfacd5cf3889
magilkurniawan <63895730+magilkurniawan@users.noreply.github.com>
2020-04-18 19:44:35 +0700

    Add files via upload

---
+++ gill.py
@@ -0,0 +1,45 @@
+import websocket,json,time,datetime,requests
+from datetime import datetime
+
+import time,requests,random
+sandi = input('Masukkan Password :')
+if sandi == 'gill'
+    print('welcome everyone>
+    else:
+        ws.close()
+xz = 1 
+status = 'tidur'
+nama = 'xyx'
+judul = 'xyz'
+timeh = datetime.today
+response3 = '{div":"hai"}'
+createdl = datetime.today()
+z = 0 
+nama = 'xyz'
+judull = 'xwx'
+timehl2 = datetime.today()
+
+
+bothauthtoken2 = '29b0534f029ff4614cffbd466a5c1b2327f65f72' 
+
+paramex = {'cv:'heimdallr'}
+##ENDLINE FUNCTION---------
+###### get url spoon ###################
+txtid = input('Masukan Link Live:')
+response = requests.get(txtid)
+urlo = response.url
+slink = urlo[34:-59]
+socketstring = ("https://id-api.spooncast.net/lives/" + slink )
+print(socketstring)
+####### Get the Token ###################
+botauthtoken2 = '29b0534f029ff4614cffbd466a5c1b2327f65f72'
+myacc = '29b0534f029ff4614cffbd466a5c1b2327f65f72'
+headO = {'Authorization': 'Token 29b0534f029ff4614cffbd466a5c1b2327f65f72'}
+'accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image'
+content-type':'application/json'}
+mypesan = '{"live_id":'+slink+',"token":"'}
+
+testka = "update pio"
+botkonek = '{"message":"Beta Version pio","useragent":"Android","event"}'
+#cbb = JSON.loads(botkonek)
+############
